bc1a641e30653c282bfbc24668a7d152c3e5aab8
Andrey Kolomoets <kolomoets.and@gmail.com>
2021-11-10 23:24:32 +0200

    fix3

--- js/main.js
+++ js/main.js
@@ -136,83 +136,83 @@ $(document).ready(function() {
     };
 
     // Video Slider
-    if ( $('.video-slider-main').length ) {
-        var $slider = $('.video-slider-main')
-            .on('init', function(slick) {
-                $('.video-slider-main').fadeIn(1000);
-            })
-            .slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: true,
-                autoplay: true,
-                lazyLoad: 'ondemand',
-                autoplaySpeed: 3000,
-                asNavFor: '.video-slider-thmb',
-                appendArrows: $('.video-slider-arrows'),
-                prevArrow: '<button id="prev" type="button" class="slider-btn"> < </button>',
-                nextArrow: '<button id="next" type="button" class="slider-btn"> > </button>'
-            });
+    // if ( $('.video-slider-main').length ) {
+    //     var $slider = $('.video-slider-main')
+    //         .on('init', function(slick) {
+    //             $('.video-slider-main').fadeIn(1000);
+    //         })
+    //         .slick({
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1,
+    //             arrows: true,
+    //             autoplay: true,
+    //             lazyLoad: 'ondemand',
+    //             autoplaySpeed: 3000,
+    //             asNavFor: '.video-slider-thmb',
+    //             appendArrows: $('.video-slider-arrows'),
+    //             prevArrow: '<button id="prev" type="button" class="slider-btn"> < </button>',
+    //             nextArrow: '<button id="next" type="button" class="slider-btn"> > </button>'
+    //         });
 
-        var $slider2 = $('.video-slider-thmb')
-            .on('init', function(slick) {
-                $('.video-slider-thmb').fadeIn(1000);
-            })
-            .slick({
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                lazyLoad: 'ondemand',
-                asNavFor: '.video-slider-main',
-                dots: false,
-                arrows: true,
-                centerMode: false,
-                focusOnSelect: true
-            });
+    //     var $slider2 = $('.video-slider-thmb')
+    //         .on('init', function(slick) {
+    //             $('.video-slider-thmb').fadeIn(1000);
+    //         })
+    //         .slick({
+    //             slidesToShow: 4,
+    //             slidesToScroll: 1,
+    //             lazyLoad: 'ondemand',
+    //             asNavFor: '.video-slider-main',
+    //             dots: false,
+    //             arrows: true,
+    //             centerMode: false,
+    //             focusOnSelect: true
+    //         });
 
-        // // Init Slider
-        $('.video-slider-thmb .slick-slide').removeClass('slick-active');
+    //     // // Init Slider
+    //     $('.video-slider-thmb .slick-slide').removeClass('slick-active');
 
-        $('.video-slider-thmb .slick-slide').eq(0).addClass('slick-active');
+    //     $('.video-slider-thmb .slick-slide').eq(0).addClass('slick-active');
 
-        $('.video-slider-main').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-            var mySlideNumber = nextSlide;
-            $('.video-slider-thmb .slick-slide').removeClass('slick-active');
-            $('.video-slider-thmb .slick-slide').eq(mySlideNumber).addClass('slick-active');
-        });
+    //     $('.video-slider-main').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    //         var mySlideNumber = nextSlide;
+    //         $('.video-slider-thmb .slick-slide').removeClass('slick-active');
+    //         $('.video-slider-thmb .slick-slide').eq(mySlideNumber).addClass('slick-active');
+    //     });
         
-        require(['js-sliderWithProgressbar'], function(slider) {
+    //     require(['js-sliderWithProgressbar'], function(slider) {
 
-            $('.video-slider-main').each(function() {
+    //         $('.video-slider-main').each(function() {
 
-                me.slider = new slider($(this), options, sliderOptions, previewSliderOptions);
+    //             me.slider = new slider($(this), options, sliderOptions, previewSliderOptions);
 
-            });
-        });
+    //         });
+    //     });
 
-        var options = {
-            progressbarSelector    : '.bJS_progressbar'
-            , slideSelector        : '.bJS_slider'
-            , previewSlideSelector : '.bJS_previewSlider'
-            , progressInterval     : ''
-            , onCustomProgressbar : function($slide, $progressbar) {}
-        }
+    //     var options = {
+    //         progressbarSelector    : '.bJS_progressbar'
+    //         , slideSelector        : '.bJS_slider'
+    //         , previewSlideSelector : '.bJS_previewSlider'
+    //         , progressInterval     : ''
+    //         , onCustomProgressbar : function($slide, $progressbar) {}
+    //     }
 
-        var sliderOptions = {
-            slidesToShow   : 1,
-            slidesToScroll : 1,
-            arrows         : true,
-            fade           : true,
-            autoplay       : true
-        }
+    //     var sliderOptions = {
+    //         slidesToShow   : 1,
+    //         slidesToScroll : 1,
+    //         arrows         : true,
+    //         fade           : true,
+    //         autoplay       : true
+    //     }
 
-        var previewSliderOptions = {
-            slidesToShow   : 3,
-            slidesToScroll : 1,
-            dots           : false,
-            focusOnSelect  : true,
-            centerMode     : true
-        }
-    }
+    //     var previewSliderOptions = {
+    //         slidesToShow   : 3,
+    //         slidesToScroll : 1,
+    //         dots           : false,
+    //         focusOnSelect  : true,
+    //         centerMode     : true
+    //     }
+    // }
 
     // Collapse
     $('#collapseBtn').click(function() {
@@ -231,38 +231,38 @@ $(document).ready(function() {
 });
 
 // Scrollbar    
-if ($(window).width() >= '801') {
-    $(document).scroll(function (e) {
-        var scrollAmount = $(window).scrollTop();
-        var documentHeight = $(document).height();
-        var windowHeight = $(window).height();
-        var scrollPercent = (scrollAmount / (documentHeight - windowHeight)) * 100;
+// if ($(window).width() >= '801') {
+//     $(document).scroll(function (e) {
+//         var scrollAmount = $(window).scrollTop();
+//         var documentHeight = $(document).height();
+//         var windowHeight = $(window).height();
+//         var scrollPercent = (scrollAmount / (documentHeight - windowHeight)) * 100;
     
-        var product = 'О продукте';
-        var properties = 'Свойства';
-        var composition = 'Состав';
-        var recommendations = 'Рекомендации';
-        var video = 'Видео';
-        var map ='Зоомагазины';
-        var partners = 'Интернет-магазины';
+//         var product = 'О продукте';
+//         var properties = 'Свойства';
+//         var composition = 'Состав';
+//         var recommendations = 'Рекомендации';
+//         var video = 'Видео';
+//         var map ='Зоомагазины';
+//         var partners = 'Интернет-магазины';
     
-        if (scrollPercent < 14){
-            var catName = product;
-        } else if ( scrollPercent < 28) {
-            var catName = properties;
-        } else if ( scrollPercent < 42) {
-            var catName = composition;
-        } else if ( scrollPercent < 56) {
-            var catName = recommendations;
-        } else if ( scrollPercent < 70) {
-            var catName = video;
-        } else if ( scrollPercent < 84) {
-            var catName = map;
-        } else {
-            var catName = partners;
-        }
+//         if (scrollPercent < 14){
+//             var catName = product;
+//         } else if ( scrollPercent < 28) {
+//             var catName = properties;
+//         } else if ( scrollPercent < 42) {
+//             var catName = composition;
+//         } else if ( scrollPercent < 56) {
+//             var catName = recommendations;
+//         } else if ( scrollPercent < 70) {
+//             var catName = video;
+//         } else if ( scrollPercent < 84) {
+//             var catName = map;
+//         } else {
+//             var catName = partners;
+//         }
         
-        $(".scrollball").css("top", "calc(" + scrollPercent + "%)");
-        $(".scrollball span").text(catName);
-    });
-};
+//         $(".scrollball").css("top", "calc(" + scrollPercent + "%)");
+//         $(".scrollball span").text(catName);
+//     });
+// };
